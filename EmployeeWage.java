735c1f5be10f07a9c3d690adef481e854d7218b0
SgOrigin999 <you@example.com>
2021-05-29 19:05:34 +0530

    Check Employee Avilablity

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,6 +2,17 @@ package com.bridgelab.day3;
 
 public class EmployeeWage {
 	public static void main(String[] args) {
+		int Full_Time=1;
+		
 		System.out.println("Welcome To Employee Wage::");
+		
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		System.out.println("Emp Check Value::"+empCheck);
+		if(empCheck==Full_Time) {
+			System.out.println("Employee is Present");
+		}else
+		{
+			System.out.println("Employee is not Present");
+		}
 	}
 }
