3239dd69abd647fb7a3e0957684216ae7417069f
SgOrigin999 <you@example.com>
2021-05-29 20:01:20 +0530

    Use Switch To Calculate Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,12 @@
 package com.bridgelab.day3;
 
 public class EmployeeWage {
-	public static int FULL_TIME = 1;
+	public static final int FULL_TIME = 1;
+	public static final int HALF_TIME = 0;
 	public static int WAGEPERHOUR = 20;
 	public static int FULL_TIME_HRS = 8;
+	public static int EMP_HRS = 8;
+	public static int TOTAL_WAGE ;
 
 	public static void calculateWage() {
 		int totalWage = 0;
@@ -38,7 +41,30 @@ public class EmployeeWage {
 		}
 	}
 
+	public  double useSwitch(int random) {
+		int empCheck = random;
+		int empHrs;
+		int empWage;
+		switch (empCheck) {
+		case HALF_TIME :
+			EMP_HRS = 4;
+			break;
+		case FULL_TIME :
+			EMP_HRS = 8;
+			break;
+		default:
+			empHrs = 0;
+		}
+		TOTAL_WAGE = WAGEPERHOUR * EMP_HRS;
+		return TOTAL_WAGE;
+	}
+	
 	public static void main(String[] args) {
+		
+		int random = (int) Math.floor(Math.random() * 10) % 2;
+		EmployeeWage emp = new EmployeeWage();
+		double totalWage=emp.useSwitch(random);
+		System.out.println("Use Switch Case::"+totalWage);
 		System.out.println("Part Time Wage::");
 		partTimeWage();
 		System.out.println("Calculate Daily Wage::");
