1721a9d4e56dd231b29c2ae6e1834643016e1786
SgOrigin999 <you@example.com>
2021-05-29 20:30:54 +0530

    Calculate Wage For Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,8 +4,9 @@ public class EmployeeWage {
 	public static final int FULL_TIME = 1;
 	public static final int HALF_TIME = 0;
 	public static int WAGEPERHOUR = 20;
-	public static int FULL_TIME_HRS = 8;
-	public static int EMP_HRS = 8;
+	public static int FULL_TIME_HRS;
+	public static int HALF_TIME_HRS;
+	public static int NUM_OF_WORKING_DAYS = 20;
 	public static int TOTAL_WAGE ;
 
 	public static void calculateWage() {
@@ -47,20 +48,43 @@ public class EmployeeWage {
 		int empWage;
 		switch (empCheck) {
 		case HALF_TIME :
-			EMP_HRS = 4;
+			empHrs = 4;
 			break;
 		case FULL_TIME :
-			EMP_HRS = 8;
+			empHrs = 8;
 			break;
 		default:
 			empHrs = 0;
 		}
-		TOTAL_WAGE = WAGEPERHOUR * EMP_HRS;
+		TOTAL_WAGE = WAGEPERHOUR * empHrs;
 		return TOTAL_WAGE;
+	}
+	
+	public static void wageForMonth() {
+		int empHrs;
+		int empWage;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case FULL_TIME:
+				empHrs = 8;
+				break;
+			case HALF_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = WAGEPERHOUR * empHrs;
+			TOTAL_WAGE  = TOTAL_WAGE  + empWage;
+		}
+		System.out.println("Total Wage::" + TOTAL_WAGE);
 	}
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Calculate  Wage For Month::");
+		wageForMonth();
 		int random = (int) Math.floor(Math.random() * 10) % 2;
 		EmployeeWage emp = new EmployeeWage();
 		double totalWage=emp.useSwitch(random);
