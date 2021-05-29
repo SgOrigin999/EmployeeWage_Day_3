63be24fda5cdfacbe0bb20a0fdf168dd2570e2fc
SgOrigin999 <you@example.com>
2021-05-29 19:25:45 +0530

    Calculate Daily Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,18 +1,37 @@
 package com.bridgelab.day3;
 
 public class EmployeeWage {
-	public static void main(String[] args) {
-		int Full_Time=1;
-		
-		System.out.println("Welcome To Employee Wage::");
-		
-		double empCheck=Math.floor(Math.random() * 10) % 2;
-		System.out.println("Emp Check Value::"+empCheck);
-		if(empCheck==Full_Time) {
+	public static int FULL_TIME = 1;
+	public static int WAGEPERHOUR = 20;
+	public static int FULL_TIME_HRS = 8;
+
+	public static void calculateWage() {
+		int totalWage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == 1) {
+			totalWage = WAGEPERHOUR * FULL_TIME_HRS;
+			System.out.println("Employee daily Wage::" + totalWage);
+		} else {
+			System.out.println("Employee daily Wage::" + totalWage);
+		}
+	}
+
+	public static void checkAvilablity() {
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		System.out.println("Emp Check Value::" + empCheck);
+		if (empCheck == FULL_TIME) {
 			System.out.println("Employee is Present");
-		}else
-		{
+		} else {
 			System.out.println("Employee is not Present");
 		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Calculate Daily Wage::");
+		calculateWage();
+		System.out.println("Check Employee Avilablity::");
+		checkAvilablity();
+		System.out.println("Welcome To Employee Wage::");
+
 	}
 }
